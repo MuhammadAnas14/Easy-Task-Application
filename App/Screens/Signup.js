1986c5c10f0b76f62345c5c75c324aa466fe986d
MuhammadAnas14 <anasm9877@gmail.com>
2021-08-16 01:55:23 +0500

    loader

---
+++ App/Screens/Signup.js
@@ -0,0 +1,14 @@
+import React, {useState, createRef} from 'react';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  Image,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+
+
