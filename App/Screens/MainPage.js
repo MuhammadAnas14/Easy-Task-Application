bb522ffaa90beaedc2c85d7c64407016e1ed2b65
TalalBhatti <talalabhatti@gmail.com>
2021-09-03 19:56:18 +0500

    Icons and Error

--- App/Screens/MainPage.js
+++ App/Screens/MainPage.js
@@ -3,6 +3,7 @@ import {View,Text,StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
   
 
@@ -10,7 +11,7 @@ function HomeStack() {
     return (
         <View style={styles.mainBody}>
         <Text>Hello, Peter</Text>
-        <Text>ALL CATEGOORIES WITH ICON and add other stacks</Text>
+        <Text>ALL CATEGORIES WITH ICON and add other stacks</Text>
         </View>
     );
 }
@@ -39,34 +40,50 @@ const MainPage = () => {
         <NavigationContainer>
         <Tab.Navigator
           initialRouteName="Feed"
-          tabBarOptions={{
-            activeTintColor: '#42f44b',
+          screenOptions={{
+            activeTintColor: '#3CAABB',
           }}>
           <Tab.Screen
             name="HomeStack"
             component={HomeStack}
             options={{
+              title: 'Home',
+              headerStyle: {
+                backgroundColor: '#3CAABB',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
               tabBarLabel: 'Home',
-            //   tabBarIcon: ({ color, size }) => (
-            //     <MaterialCommunityIcons
-            //       name="home"
-            //       color={color}
-            //       size={size}
-            //     />
-            //   ),
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons
+                  name="home"
+                  color={color}
+                  size={size}
+                />
+              ),
             }}  />
           <Tab.Screen
             name="SettingsStack"
             component={SettingsStack}
             options={{
+                title: 'Account Settings',
+                headerStyle: {
+                  backgroundColor: '#3CAABB',
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                  fontWeight: 'bold',
+                },
               tabBarLabel: 'Settings',
-            //   tabBarIcon: ({ color, size }) => (
-            //     <MaterialCommunityIcons
-            //       name="settings"
-            //       color={color}
-            //       size={size}
-            //     />
-            //   ),
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons
+                  name="account-settings"
+                  color={color}
+                  size={size}
+                />
+              ),
             }} />
         </Tab.Navigator>
       </NavigationContainer>
