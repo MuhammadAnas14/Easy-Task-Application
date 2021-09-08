81463a4a5b5b053ea57124f861121154e0769e21
MuhammadAnas14 <anasm9877@gmail.com>
2021-09-09 00:51:52 +0500

    minor changes

--- App.js
+++ App.js
@@ -5,12 +5,11 @@ import SignUp from "./App/Screens/Signup"
 import Login from "./App/Screens/Login"
 import SplashScreen from "./App/Screens/Splash.js"
 import OtpScreen from './App/Screens/otp';
-import ForgetPassword from './App/Screens/ForgetPassword'
-import MainPage from './App/Screens/MainPage'
 import PostedTask from './App/Screens/PostedTask'
 import MyTask from './App/Screens/MyTask'
 import Feedback from './App/Screens/Feedback';
 import ScreenManager from './App/Screens/ScreenManager'
+import ForgetPasswordScreen from './App/Screens/ForgetPassword';
 
     
 
