aab4febb552f2a5d6719f17b6f5cc5d75d195b0a
TalalBhatti <talalabhatti@gmail.com>
2021-11-03 22:48:11 +0500

    commit

--- App/Components/Url.js
+++ App/Components/Url.js
@@ -1,16 +1,6 @@
 
+const Url = "http:/192.168.1.107:8000"
 
-<<<<<<< HEAD
-const Url = "http:/192.168.1.101:8080"
-=======
-<<<<<<< HEAD
-const Url = "http:/192.168.1.107:8080"
-=======
-// const Url = "http:/192.168.0.111:8000"
-
-const Url = "https://fyp-easy-app.herokuapp.com"
-
->>>>>>> 403647b164863740d78d06f23574f14c9372f47f
->>>>>>> d1c4ba1a9591a5671f4b1b7e08974aea1eb49b4f
+//const Url = "https://fyp-easy-app.herokuapp.com"
 
 export default Url
