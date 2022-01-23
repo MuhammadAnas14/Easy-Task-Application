127ca3bbbc86f1dfc4f65042e78da7df6f09c8f2
TalalBhatti <talalabhatti@gmail.com>
2022-01-23 23:19:59 +0500

    Ui changes

--- App/Screens/Task/TaskScreen.js
+++ App/Screens/Task/TaskScreen.js
@@ -5,18 +5,23 @@ import {
   View,
   FlatList,
   Image,
+  ScrollView,
   TouchableOpacity,
 } from "react-native";
 import * as Progress from 'react-native-progress';
 import Data from '../Data/TaskDetails.json'
+import Entypo from 'react-native-vector-icons/Entypo';
+
 
 const TaskDetails = () => {
 
   return (
     <View style={styles.container}>
+                   <ScrollView
+          keyboardShouldPersistTaps="handled">
       {/* TAsk Bar */}
       <View style={styles.BarContainer}>
-      <Progress.Bar progress={0.17} width={350} height={30} />
+      <Progress.Bar progress={0.43} width={350} height={30} />
       <Text style={styles.BarText}>OPEN     ASSIGNED     COMPLETED     REVIEWED</Text>
       </View>
 
@@ -25,15 +30,32 @@ const TaskDetails = () => {
         <Text style={styles.heading}>{Data.title}</Text>
       </View>
       {/* Profile of the Poster */}
-      <View>
-        <Text>
-          AVATAR, {Data.location}, {Data.duedate}, {Data.offers}
-        </Text>
-      </View>
+      <View style={styles.ProfCont}>
+      <Image source={{uri:Data.photo}}  style={{width:60, height:60,borderRadius:30,marginTop:10}} />
+      <View style={{flex:1,padding:15}}>
+        <Text style={{fontWeight:"bold",fontSize:18}}>Posted By</Text>
+        <Text style={{fontSize:15,marginTop:10}}>{Data.name}</Text>
+        </View>
+        </View>
+        <View style={styles.ProfCont}>
+        <Entypo style={styles.icons} size={35} name="location-pin" />
+      <View style={{flex:1,padding:15}}>
+        <Text style={{fontWeight:"bold",fontSize:18}}>Location</Text>
+        <Text style={{fontSize:15,marginTop:10}}>{Data.location}</Text>
+        </View>
+        </View>
+        <View style={styles.ProfCont}>
+        <Entypo style={styles.icons} size={35} name="calendar" />
+      <View style={{flex:1,padding:15}}>
+        <Text style={{fontWeight:"bold",fontSize:18}}>Due Date</Text>
+        <Text style={{fontSize:15,marginTop:10}}>{Data.duedate}</Text>
+        </View>
+        </View>
       {/* This is For the OFFER BOX */}
       <View style={[styles.listItem]}>
         <View style={{ alignItems: "center", flex: 1 }}>
           <Text style={{ fontWeight: "bold", padding: 10 }}>Task Budget Estimate</Text>
+          <Text style={{ fontWeight: "bold", textAlign: 'right' }}>Total Bids : {Data.offers}</Text>
           <Text style={{ fontWeight: "bold", padding: 10 }}>{Data.cost}</Text>
         </View>
         <View style={styles.verifyButton}>
@@ -41,18 +63,20 @@ const TaskDetails = () => {
           style={{ justifyContent: "center", alignItems: "center" }}
           // onPress={handlerEmailOtp}
           >
-            <Text style={[styles.buttoncolor && styles.warning]}>
+            <Text style={styles.buttoncolor}>
              MAKE OFFER
             </Text>
           </TouchableOpacity>
         </View>
       </View>
       {/* Task Details */}
-      <View>
-        <Text style={{fontSize:15, fontWeight:"bold", marginLeft:13,marginTop:10}}>Task Details</Text>
-        <Text style={{fontSize:14,margin:10}}>{Data.details}</Text>
+      <View style={{marginLeft:30}}>
+        <View style={styles.bottomrad}>
+        <Text style={{fontSize:20, fontWeight:"bold", marginLeft:13,marginTop:10}}>Task Details</Text>
+        </View>
+        <Text style={{fontSize:14,margin:10,marginBottom:50}}>{Data.details}</Text>
       </View>
-
+      </ScrollView>
     </View>
   );
 };
@@ -68,9 +92,10 @@ const styles = StyleSheet.create({
     width: "80%",
     alignSelf: "center",
     borderRadius: 5,
-    margin: 10,
+    margin: 40,
   },
   verifyButton: {
+    flex:1,
     justifyContent: "center",
     textAlign: "center",
     flexDirection: "row",
@@ -79,7 +104,7 @@ const styles = StyleSheet.create({
   buttoncolor:{
     color: "white", 
     fontWeight: "bold",
-    backgroundColor: "red",
+    backgroundColor: "green",
     padding: 10,
   },
   warning:{
@@ -102,10 +127,37 @@ const styles = StyleSheet.create({
     color: "black"
   },
   heading:{
-  fontSize: 15,
+  fontSize: 20,
   fontWeight: "bold",
+  color:'black',
   textAlign: "center",
   margin: 10,
+  borderRadius: 12,
+  padding:20,
+  borderColor: 'white',
+  backgroundColor:'lightblue',
+  borderWidth: 2
+  },
+  location:{
+    flexDirection:'row'
+  },
+  icons:{
+      margin:5,
+      padding:10,
+  },
+  ProfCont:{
+    marginLeft:30,
+    flexDirection:'row',
+    textDecorationLine:'underline',
+    borderBottomColor: 'black',
+    borderBottomEndRadius: 35,
+    borderBottomWidth: 2,
+  },
+  bottomrad:{
+    borderBottomColor: 'black',
+    borderBottomEndRadius: 35,
+    borderBottomWidth: 2,
+    paddingBottom:10,
   }
 });
 export default TaskDetails;
