8e893194854209a63e584b3c456a967438008538
MuhammadAnas14 <anasm9877@gmail.com>
2022-02-03 23:53:46 +0500

    online fix

--- App/Screens/Task/PostedTask.js
+++ App/Screens/Task/PostedTask.js
@@ -10,10 +10,20 @@ import { useNavigation } from '@react-navigation/native';
 function Item({ item}) {
 
   const navigation = useNavigation();
+  const [locationMethod,setLocationMethod] = useState("Online");
 
   const tracklocation = () => {
     navigation.navigate('Task Details',{item})
-  }    
+  }
+  useEffect(() => {
+  
+  if (item.taskLocation){
+    setLocationMethod(item.taskLocation)
+  }
+  else{
+    setLocationMethod("Online")
+  }
+  }, [])
   return (
     <View style={styles.listItem}>
       <Image source={{uri:`data:image/jpg;base64,${item.userPhoto}`}}  style={{width:60, height:60,borderRadius:30}} />
@@ -21,7 +31,7 @@ function Item({ item}) {
         <Text style={{fontWeight:"bold"}}>{item.taskName}</Text>
         <View style={styles.location}>
             <Entypo style={styles.icons}  name="location-pin" />
-            <Text>{item.taskLocation}</Text>
+            <Text>{locationMethod}</Text>
         </View>
         <Text>{item.comments} Comments</Text>
       </View>
