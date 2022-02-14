85a2b6949938fde172d196e7d78182e5d40c86a8
MuhammadAnas14 <anasm9877@gmail.com>
2022-02-15 00:58:25 +0500

    bug

--- App/Components/Url.js
+++ App/Components/Url.js
@@ -1,7 +1,7 @@
 
 
-// const Url = "http://192.168.0.113:8080"
+const Url = "http://192.168.0.113:8080"
 
-const Url = "https://fyp-easy-app.herokuapp.com"
+// const Url = "https://fyp-easy-app.herokuapp.com"
 
 export default Url
